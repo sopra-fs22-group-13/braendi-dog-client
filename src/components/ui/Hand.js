07c3ed5b2275511b779ef6cc72e3788918665c55
shzeng <shitao.zeng@uzh.ch>
2022-04-04 20:05:23 +0200

    added initial styling for hand

--- src/components/ui/Hand.js
+++ src/components/ui/Hand.js
@@ -5,10 +5,14 @@ import PropTypes from "prop-types";
  * Hand component to contain the cards of the player
  */
 
+// hand houses the cards of the player
 const Hand = props => {
     let playerCards = props.playerCards;
-
-
+    return(
+        <div>
+            
+        </div>
+    );
 }
 
 Hand.PropTypes = {

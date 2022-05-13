de21668b9a926234f0e331fbb3c6688f3d203afd
BlackStar5044 <simona.borghi@uzh.ch>
2022-05-14 00:52:41 +0200

    playerInfo final restruct (hopefully)

--- src/components/ui/PlayerInfo.js
+++ src/components/ui/PlayerInfo.js
@@ -10,9 +10,28 @@ import VolumeOffIcon from '@mui/icons-material/VolumeOff';
 import { startListening, VoiceChatManager } from 'components/voice/voiceChat';
 
 
+const Player = props => {
+
+    return(
+        <div {...props}>
+            <Avatar src={process.env.PUBLIC_URL + "/resources/avatar/" + props.path + ".png"} sx={{height: "7vh", width: "7vh"}}/>
+            <div className= "playerinfo-container indicators">
+                {props.volumeUp ? <VolumeOffIcon/> : <VolumeUpIcon/>}
+                <label> {props.username} </label>
+            </div>
+        </div>
+    );
+}
+
+Player.propTypes = {
+  path: PropTypes.string,
+  volumeUp: PropTypes.bool,
+  username: PropTypes.string
+};
+
 const PlayerInfo = props => {
 
-    const [info, setInfo] = useState(null);
+    const [users, setUsers] = useState(null);
     useEffect(() => {
         async function fetchDataLobby() {
           try {
@@ -21,7 +40,7 @@ const PlayerInfo = props => {
                   'Authorization': "Basic " + localStorage.getItem("token")
               }
           });
-          setInfo(response.data);
+          setUsers(response.data);
           } catch (error) {
               addError("Could not fetch players", 5000);
               console.error("Details:", error);
@@ -41,42 +60,20 @@ const PlayerInfo = props => {
     let myPlayer;
     let otherPlayers;
 
-
-
+    //<Player onClick={() => muteMe()} path={""+ users[0].avatar} volumeUp={muteStateMe} username={users[0].username}/>
     myPlayer = (
-        <div onClick={() => muteMe()}>
-            <Avatar src="/resources/avatar/1.png" sx={{height: "7vh", width: "7vh"}}/>
-            <div className= "playerinfo-container indicators">
-                {muteStateMe ? <VolumeOffIcon/> : <VolumeUpIcon/> }
-                <label> username </label>
-            </div>
-        </div>
+            <Player onClick={() => muteMe()} path={""+1} volumeUp={muteStateMe} username="test"/>
     )
 
 
+    /* {users.slice(1).map(user => (
+        <Player onClick={() => muteOthers()} path={""+ user.avatar} volumeUp={muteStateOthers} username= {user.username}/>
+     ))}*/ 
     otherPlayers = (
         <>
-        <div onClick={() => muteOthers()}>
-             <Avatar src="/resources/avatar/2.png" sx={{height: "7vh", width: "7vh"}}/>
-             <div className= "playerinfo-container indicators">
-                 {muteStateOthers ? <VolumeOffIcon/> : <VolumeUpIcon/> }
-                 <label> username </label>
-             </div>
-         </div>
-        <div onClick={() => muteOthers()}>
-             <Avatar src="/resources/avatar/3.png" sx={{height: "7vh", width: "7vh"}}/>
-             <div className= "playerinfo-container indicators">
-                 {muteStateOthers ? <VolumeOffIcon/> : <VolumeUpIcon/> }
-                 <label> username </label>
-             </div>
-         </div>
-        <div onClick={() => muteOthers()}>
-             <Avatar src="/resources/avatar/4.png" sx={{height: "7vh", width: "7vh"}}/>
-             <div className= "playerinfo-container indicators">
-                 {muteStateOthers ? <VolumeOffIcon/> : <VolumeUpIcon/> }
-                 <label> username </label>
-             </div>
-         </div>
+        <Player onClick={() => muteOthers()} path={""+2} volumeUp={muteStateOthers} username="test"/>
+        <Player onClick={() => muteOthers()} path={""+3} volumeUp={muteStateOthers} username="test"/>
+        <Player onClick={() => muteOthers()} path={""+4} volumeUp={muteStateOthers} username="mesmmongna"/>
         </>
     )
 
